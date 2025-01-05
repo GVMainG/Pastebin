a0339ffdc549eb05f19fc8f255bc8ca934154e33
Georgeiy <GVMainG@yandex.ru>
2025-01-06 00:33:03 +0300

    Delete TextService

--- Backend/TextService/Business/Models/TextDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TextService.Business.Models
-{
-    public class TextDto
-    {
-        public string Content { get; set; }
-        public DateTime? ExpirationDate { get; set; }
-    }
-}

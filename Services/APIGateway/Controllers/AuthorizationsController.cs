3e0889bb5109b53ca3df46000edfbb2b3c35d4e5
Georgeiy <GVMainG@yandex.ru>
2025-02-23 00:46:13 +0300

    Контроллер авторизации

---
+++ Services/APIGateway/Controllers/AuthorizationsController.cs
@@ -0,0 +1,21 @@
+﻿using APIGateway.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIGateway.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorizationsController : ControllerBase
+{
+    [HttpGet("auth/register")]
+    public OkObjectResult Registration([FromBody] RegistrationRequest request)
+    {
+        return Ok(Guid.NewGuid());
+    }
+
+    [HttpGet("auth/login ")]
+    public OkObjectResult Login([FromBody] RegistrationRequest request)
+    {
+        return Ok(true);
+    }
+}
---
+++ Services/APIGateway/Models/RegistrationRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIGateway.Models
+{
+    public class RegistrationRequest
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
